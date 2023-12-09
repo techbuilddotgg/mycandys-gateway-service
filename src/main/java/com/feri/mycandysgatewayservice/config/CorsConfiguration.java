dfb5eb955ac24043fe38a963c3ed40e3907bd199
Matevz <semprimoznik.matevz@gmail.com>
2023-12-09 01:53:47 +0100

    fix: cors

--- src/main/java/com/feri/mycandysgatewayservice/config/CorsConfiguration.java
+++ src/main/java/com/feri/mycandysgatewayservice/config/CorsConfiguration.java
@@ -19,6 +19,7 @@ public class CorsConfiguration extends
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         corsConfig.addAllowedHeader("*");
+        corsConfig.applyPermitDefaultValues();
 
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
